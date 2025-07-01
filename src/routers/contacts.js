fdbe4caeef2c4eed81b60ec9f290219fa4fa0ee2
Natalia <holovshyk@gmail.com>
2025-07-01 15:48:09 +0300

    feat: implement user authentication with session management; add user and session models, validation schemas, and authentication middleware

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,10 +13,13 @@ import {
   updateContactShema,
 } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
+router.use(authenticate);
 router.get('/', ctrlWrapper(getContactsController));
+
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post(
   '/',
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -14,6 +14,7 @@ export function setupServer() {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
@@ -23,10 +24,14 @@ export function setupServer() {
     }),
   );
 
-  app.use('/contacts', contactsRouter);
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: `Server is running, use endpoint '/contacts' and '/contacts/:contactId'`,
+    });
+  });
 
+  app.use(router);
   app.use(notFoundHandler);
-
   app.use(errorHandler);
 
   app.listen(PORT, () => {
